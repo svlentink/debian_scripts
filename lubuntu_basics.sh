fe86dca4a91d2b339e0ce317f5139d6b1cb1a6ce
SV Lentink <svlentink@gmail.com>
2015-09-06 15:41:37 +0200

    simplified script

--- lubuntu_basics.sh
+++ lubuntu_basics.sh
@@ -5,23 +5,10 @@ if [ -z $(which xcalib) ]; then
 	exit
 fi
 
-function changeNrOfDesktops { #tested on lubuntu 15.04
-	NR=$1
-	if [ -z $1 ]; then
-		NR=3
-	fi
-	sed -i 's/<number>2/<number>'$NR'/g' ~/.config/openbox/*.xml
-}
-
 function colorInvertKeybindLubuntu { # tested on lubuntu 15.04
 	# xcalib -s 0 -i -a
 	sed -i 's/<!-- Keybindings for window switching -->/<!--Keybindings for window switching--><keybind key="Super-N"><action name="Execute"><command>xcalib -a -i<\/command><\/action><\/keybind>/g' ~/.config/openbox/*.xml
 	openbox --reconfigure
-}
-
-function invertColorsLXterminal { #tested on lubuntu 15.04
-	sed -i 's/bgcolor=#000000000000/bgcolor=#ffffffffffff/g' ~/.config/lxterminal/*.conf
-	sed -i 's/fgcolor=#aaaaaaaaaaaa/fgcolor=#333333333333/g' ~/.config/lxterminal/*.conf
 }
 
 function setSleepButton {#
@@ -49,47 +36,11 @@ function suspendOnLowPowerDOESNOTWORK {
 	sed -i 's/'$GREP'/'$SED'/g' $DEST
 }
 
-
-function INSTALLbasics { # tested on lubuntu 15.04
-	sudo apt-get install shotwell -y
-	sudo apt-get install htop
-	sudo apt-get install vlc -y
-	sudo apt-get install vim
-	sudo apt-get install gnome-system-monitor -y
-	sudo apt-get remove pidgin -y			# instant message
-	sudo apt-get remove sylpheed -y		# mail client
-}
-
-function INSTALLoffice { # tested on lubuntu 15.04
-	sudo apt-get remove abiword -y
-	sudo apt-get install libreoffice -y
-}
-
-function runChromeOnBoot { # tested on lubuntu 15.04
-	# http://dottech.org/118513/how-to-autostart-autorun-a-program-on-boot-in-ubuntu-guide/
-	sudo ln -s /usr/share/applications/google-chrome.desktop ~/.config/autostart
-}
-
 function depricatedINSTALLarandr {
 	echo DEPRICATED, from lubuntu 15.04 this is not needed anymore
 #	sudo apt-get install arandr #for multiple screens
 }
 
-function addMonitorsToTaskbar { # tested on lubuntu 15.04
-	local dest=~/.config/lxpanel/Lubuntu/panels/panel
-
-	echo Adding system monitor to task bar
-	echo "Plugin {" >> $dest
-	echo "type=monitors" >> $dest
-	echo "Config {" >> $dest
-	echo "DisplayCPU=1" >> $dest
-	echo "DisplayRAM=1" >> $dest
-	echo "Action=gnome-system-monitor" >> $dest
-	echo "CPUColor=#00FFFF" >> $dest
-	echo "RAMColor=#FFFF00" >> $dest
-	echo "}" >> $dest
-	echo "}" >> $dest
-}
 function addBatteryToTaskbar { # tested on lubuntu 15.04
 	local dest=~/.config/lxpanel/Lubuntu/panels/panel
 
@@ -100,3 +51,9 @@ function addBatteryToTaskbar { # tested on lubuntu 15.04
 	echo "}" >> $dest
 	echo "}" >> $dest
 }
+
+colorInvertKeybindLubuntu
+setSleepButton
+suspendOnLowPower
+sleepWhenLidCloses
+addBatteryToTaskbar
