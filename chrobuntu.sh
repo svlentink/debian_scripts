f40e4bd55eb6cf61745cec64b4c0bdd5e6d80231
SV Lentink <svlentink@gmail.com>
2015-09-06 16:18:35 +0200

    Update chrobuntu.sh

--- chrobuntu.sh
+++ chrobuntu.sh
@@ -3,7 +3,7 @@
 function runFromUrl {
 	wget --no-cache -O /tmp/tmpDownloaded.sh $1
 	sudo chmod +x /tmp/tmpDownloaded.sh
-	sh /tmpDownloaded.sh $2 $3 $4 $5 $6 $7 $8 $9
+	/tmp/tmpDownloaded.sh $2 $3 $4 $5 $6 $7 $8 $9
 }
 
 runFromUrl https://raw.githubusercontent.com/svlentink/debian_scripts/master/ubuntu_basics.sh
