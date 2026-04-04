a70547a52e3b9ddc32afe788d567c166752b7ecd
SanderLentink.nl <git@lent.ink>
2026-04-04 20:45:56 +0200

    persist mac

--- fs-root/usr/local/bin/set-host-specifics.sh
+++ fs-root/usr/local/bin/set-host-specifics.sh
@@ -3,13 +3,17 @@ set -ue
 
 . /host.env
 
-hostnamectl set-hostname "$HOSTNAME"
-
 # The first part of a MAC address is the vendor id
 # therefor for the bridge we use the one from the primary ethernet port
 # so the MAC address will be easier to trace to the right type of device
 MAC_PREFIX="`cat /sys/class/net/e*0/address|cut -c 1-8`"
 
-if [[ -z "`grep MACAddress /etc/systemd/network/br0.netdev`" ]]; then
-    echo "MACAddress=$MAC_PREFIX:$MAC_POSTFIX" >> /etc/systemd/network/br0.netdev
-fi
+#if [[ -z "`grep MACAddress /etc/systemd/network/br0.netdev`" ]]; then
+mkdir -p /etc/systemd/network/br0.netdev.d/
+
+cat << EOF > /etc/systemd/network/br0.netdev.d/mac.conf
+[NetDev]
+MACAddress=$MAC_PREFIX:$MAC_POSTFIX
+EOF
+
+hostnamectl set-hostname "$HOSTNAME"
